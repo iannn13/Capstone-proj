ae698a59d864867b7b019197edaaa189713b33f1
iannn13 <iankarlmusniii@gmail.com>
2024-11-13 15:27:09 +0800

    main menu new/load game

--- Assets/MAIN MENU/loadANDnew.cs
+++ Assets/MAIN MENU/loadANDnew.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Import for Button interaction
 
-public class loadANDnew : MonoBehaviour
+public class LoadAndNew : MonoBehaviour
 {
-
     [Header("LOAD")]
     [SerializeField] private GameObject loadtxt;
     [SerializeField] private GameObject loadbutton;
@@ -13,13 +13,29 @@ public class loadANDnew : MonoBehaviour
     [SerializeField] private GameObject newtxt;
     [SerializeField] private GameObject newbutton;
 
-    void Awake() {
+    private void Awake()
+    {
+        // Initially hide text elements
         loadtxt.SetActive(false);
         newtxt.SetActive(false);
         newbutton.SetActive(false);
         loadbutton.SetActive(false);
+
+        // Check if a save file exists
+        string path = System.IO.Path.Combine(Application.persistentDataPath, "playerData.json");
+
+        if (System.IO.File.Exists(path))
+        {
+            // Enable the load button if save file exists
+            loadbutton.SetActive(true);
+        }
+        else
+        {
+            // Disable the load button if no save file is found
+            loadbutton.GetComponent<Button>().interactable = false;
+        }
     }
-    
+
     public void Load()
     {
         loadtxt.SetActive(true);
